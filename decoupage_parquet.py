717647bb12073138f528ce8da96b275ae5d601eb
Pierlou <pierlou.ramade@data.gouv.fr>
2023-07-03 11:18:32 +0200

    fix: clean up

--- decoupage_parquet.py
+++ decoupage_parquet.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-path_in = r"C:\Users\Dinum\Documents\Projets\Elections\work\table_adresses.parquet"
+path_in = "./../work/table_adresses.parquet"
 df = pd.read_parquet(path_in)
 df['dep_bv'] = df['code_commune_ref'].apply(lambda s: s[:3] if s[:2]=='97' else s[:2])
 
